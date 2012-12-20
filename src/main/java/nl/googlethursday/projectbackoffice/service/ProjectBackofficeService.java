c9aa7822c5e75171b718592a137bd222523c5807
googlethursday <bagger@vfrmania.eu>
2012-12-20 16:02:43 +0100

    Commit from JBoss Tools

--- src/main/java/nl/googlethursday/projectbackoffice/service/ProjectBackofficeService.java
+++ src/main/java/nl/googlethursday/projectbackoffice/service/ProjectBackofficeService.java
@@ -1,36 +1,30 @@
 package nl.googlethursday.projectbackoffice.service;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
-
-import com.mongodb.Mongo;
 
 import nl.googlethursday.projectbackoffice.entity.Project;
 
 /**
  * Service tbv ophalen en opslaan van Project entiteiten
+ * 
  * @author rodo
- *
+ * 
  */
 @Stateless
 public class ProjectBackofficeService {
-/**
- * TODO wegschrijven in db
- */
+	/**
+	 * TODO wegschrijven in db
+	 */
 	List<Project> projectList;
 
 	@EJB
 	MongoDBService service;
-	
-	public ProjectBackofficeService() {
 
+	public ProjectBackofficeService() {
 		service.openMongoConnection();
-		
-		
 		projectList = new ArrayList<Project>();
 		Project p = new Project("naamProject", "omschrijvingProject", "projectLeider");
 		projectList.add(p);
@@ -41,8 +35,8 @@ public class ProjectBackofficeService {
 	}
 
 	public Project getProject(int id) {
-		
-		if (id>=projectList.size()){
+
+		if (id >= projectList.size()) {
 			return null;
 		}
 		return (projectList.get(id));
@@ -57,34 +51,35 @@ public class ProjectBackofficeService {
 		return projectList;
 	}
 
-	public boolean updateProjectInList(Project project){
+	public boolean updateProjectInList(Project project) {
 		String projectnaam = project.getProjectNaam();
-		
-		//zoek project
+
+		// zoek project
 		for (Project proj : projectList) {
-		
+
 			// haal projectnaam uit de list op
 			String onderhandenProjectNaam = proj.getProjectNaam();
-			
-			// komt de aangeleverde projectnaam overeen met het onderhanden projectelement?
-			if (projectnaam.equals(onderhandenProjectNaam)){
-				//naam gevonden, update project
-				proj=project;
-				
+
+			// komt de aangeleverde projectnaam overeen met het onderhanden
+			// projectelement?
+			if (projectnaam.equals(onderhandenProjectNaam)) {
+				// naam gevonden, update project
+				proj = project;
+
 				// geef boolean=true terug
 				return true;
 			}
-			
-					
+
 		}
 		// project niet gevonden
 		return false;
 
 	}
-	
-	public void opslaanProjectInList(Project project){
+
+	public void opslaanProjectInList(Project project) {
 		projectList.add(project);
 	}
+
 	public void setProjectList(List<Project> projectList) {
 		this.projectList = projectList;
 	}
