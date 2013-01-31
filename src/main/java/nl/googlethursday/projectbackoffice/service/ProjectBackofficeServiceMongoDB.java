e4ce4c9f49dca7f632a5c1be86cfe568ce03d173
googlethursday <bagger@vfrmania.eu>
2013-01-31 15:39:35 +0100

    Commit from JBoss Tools

--- src/main/java/nl/googlethursday/projectbackoffice/service/ProjectBackofficeServiceMongoDB.java
+++ src/main/java/nl/googlethursday/projectbackoffice/service/ProjectBackofficeServiceMongoDB.java
@@ -205,27 +205,32 @@ public class ProjectBackofficeServiceMongoDB {
 	 * @return
 	 */
 	private Integer bepaalId() {
-		System.out.println("bepaal id");
-		
-		collProjectId = db.getCollection(COLLECTIONNAME_ID);
-
-		System.out.println(collProjectId);
-		
-		// indien niet gevonden, zet er '1' in
-		if (collProjectId.count() == 0) {
-			System.out.println("niets gevonden");
-			BasicDBObject document = new BasicDBObject();
-			document.put("sleutel", new Integer(1));
-			collProjectId.insert(document);
-			sleutel = 1;
-		}
-
-		System.out.println("volgende stap");
-		
-		// ophalen meest recente waarde
-		DBCursor cursor = coll.find();
-		sleutel = (Integer) cursor.next().get("sleutel");
-		System.out.println("sleutel wordt: "+sleutel);
-		return sleutel;
+		return new Integer(getNextId(db, "seq"));
+	}
+	
+	
+	/**
+	 * Get the next unique ID for a named sequence.
+	 * @param db Mongo database to work with
+	 * @param seq_name The name of your sequence (I name mine after my collections)
+	 * @return The next ID
+	 */
+	public static String getNextId(DB db, String seq_name) {
+	    String sequence_collection = "seq"; // the name of the sequence collection
+	    String sequence_field = "seq"; // the name of the field which holds the sequence
+	 
+	    DBCollection seq = db.getCollection(sequence_collection); // get the collection (this will create it if needed)
+	 
+	    // this object represents your "query", its analogous to a WHERE clause in SQL
+	    DBObject query = new BasicDBObject();
+	    query.put("_id", seq_name); // where _id = the input sequence name
+	 
+	    // this object represents the "update" or the SET blah=blah in SQL
+	    DBObject change = new BasicDBObject(sequence_field, 1);
+	    DBObject update = new BasicDBObject("$inc", change); // the $inc here is a mongodb command for increment
+	 
+	    // Atomically updates the sequence field and returns the value for you
+	    DBObject res = seq.findAndModify(query, new BasicDBObject(), new BasicDBObject(), false, update, true, true);
+	    return res.get(sequence_field).toString();
 	}
 }
