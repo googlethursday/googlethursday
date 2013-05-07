5c32321badf1767501bc707d61bbb98b689ec2df
googlethursday <bagger@vfrmania.eu>
2013-05-07 22:45:05 +0200

    Commit from JBoss Tools

--- src/main/java/nl/googlethursday/projectbackoffice/service/mongodb/dao/ProjectDAO.java
+++ src/main/java/nl/googlethursday/projectbackoffice/service/mongodb/dao/ProjectDAO.java
@@ -1,5 +1,6 @@
 package nl.googlethursday.projectbackoffice.service.mongodb.dao;
 
+
 import nl.googlethursday.projectbackoffice.entity.Project;
 
 import org.bson.types.ObjectId;
@@ -8,6 +9,11 @@ import com.google.code.morphia.Morphia;
 import com.google.code.morphia.dao.BasicDAO;
 import com.mongodb.Mongo;
 
+/**
+ * ProjectDAO
+ * @author rodo
+ *
+ */
 public class ProjectDAO extends BasicDAO<Project, ObjectId>{
 
 	protected ProjectDAO(Mongo mongo, Morphia morphia, String dbName) {
